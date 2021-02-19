b5f86a112e68feaee2d873a799440b1b43863d34
Owen Cannon <owen.cannon1@gmail.com>
2021-02-19 17:44:25 -0500

    Converted class to functional component and added shape selection toggle

--- src/Components/Canvas/Canvas.tsx
+++ src/Components/Canvas/Canvas.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
 import "./Canvas.css";
-// import P5 from 'p5';
-// import p5 from 'p5';
 import 'p5';
 import '../../Types/Figures';
 import { AnimatedFigure, CircleFigure, SquareFigure, TriangleFigure } from '../../Types/ProcessingFigures';
@@ -9,70 +6,48 @@ import { CanvasSettings, SelectedShape, SelectedAnimation } from '../../Types/Fi
 import P5Wrapper from 'react-p5-wrapper';
 import 'react-p5-wrapper';
 
-interface ICanvasProps {
-    canvasSettings?: CanvasSettings
+function sketch (p) {
+    let figs: AnimatedFigure[] = [];
+    let selectedFigure = SelectedShape.None;
+
+    p.setup = function () {
+        p.createCanvas(1000, 500);
+        figs = [];
+    }
+
+    p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
+        selectedFigure = props.canvasSettings.selectedFigure;
+    }
+
+    p.draw = function () {
+        p.background(204);
+        p.fill(100);
+        figs.forEach(fig => {
+            fig.update(3);
+            fig.display();
+        });
+
+        p.mousePressed = function () {
+            switch(selectedFigure) {
+                case SelectedShape.Circle:
+                    let newCirc = new CircleFigure(p.mouseX, p.mouseY, -0.02, 50, p);
+                    figs.push(newCirc);
+                    break;
+                case SelectedShape.Rectangle:
+                    let newSquare = new SquareFigure(p.mouseX, p.mouseY, -0.02, 90, p);
+                    figs.push(newSquare);
+                    break;
+                case SelectedShape.Triangle:
+                    let newTriangle = new TriangleFigure(p.mouseX, p.mouseY, -0.02, 90, p);
+                    figs.push(newTriangle);
+                    break;
+            }
+            // prevent default
+            return false;
+        }
+    }
 }
 
-interface ICanvasState {
-
-}
-
-export default class Canvas extends React.Component<ICanvasProps, ICanvasState> {
-    constructor(props) {
-        super(props);
-    }
-    
-    //TODO: Create new types module to create types for 
-    //      functions defined by the P5Wrapper module.
-    sketch = (p) => {
-        let figs: AnimatedFigure[] = [];
-        let selectedFigure = SelectedShape.None;
-
-        p.setup = function () {
-            p.createCanvas(1000, 500);
-            figs = [];
-        }
-        
-        p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
-            if (props.canvasSettings.selectedFigure) {
-                selectedFigure = props.canvasSettings.selectedFigure;
-            }
-        }
-        
-        p.draw = function () {
-            p.background(204);
-            p.fill(100);
-            figs.forEach(fig => {
-                fig.update(3);
-                fig.display();
-            });
-            
-            p.mousePressed = function () {
-                switch(selectedFigure) {
-                    case SelectedShape.Circle:
-                        let newCirc = new CircleFigure(p.mouseX, p.mouseY, -0.02, 50, p);
-                        figs.push(newCirc);
-                        break;
-                    case SelectedShape.Rectangle:
-                        let newSquare = new SquareFigure(p.mouseX, p.mouseY, -0.02, 90, p);
-                        figs.push(newSquare);
-                        break;
-                    case SelectedShape.Triangle: 
-                        let newTriangle = new TriangleFigure(p.mouseX, p.mouseY, -0.02, 90, p);
-                        figs.push(newTriangle);
-                        break;
-                }  
-                // prevent default
-                return false;
-            }
-        }
-    }
-                
-    // componentDidMount() {
-        // this.myP5 = new p5 (this.sketch, this.myRef.current);
-    // }
-
-    render() {
-        return <P5Wrapper sketch={this.sketch} canvasSettings={this.props.canvasSettings}/>;
-    }
+export default function Canvas(props) {
+    return <P5Wrapper sketch={sketch}  canvasSettings={props.canvasSettings}/>;
 }
