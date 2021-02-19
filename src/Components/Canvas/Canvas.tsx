5191e9ade2f2a1040500600cc39e21aec60f239c
claban3 <corlaban@umich.edu>
2021-02-19 16:36:53 -0500

    fix many canvas issue by using react-p5-wrapper

--- src/Components/Canvas/Canvas.tsx
+++ src/Components/Canvas/Canvas.tsx
@@ -6,64 +6,87 @@ import 'p5';
 import '../../Types/Figures';
 import { AnimatedFigure, CircleFigure, SquareFigure, TriangleFigure } from '../../Types/ProcessingFigures';
 import { CanvasSettings, SelectedShape, SelectedAnimation } from '../../Types/Figures';
+import PropTypes from 'prop-types';
+import P5Wrapper from 'react-p5-wrapper';
+import 'react-p5-wrapper';
 
-export default function Canvas(props) {
-    let myP5: P5;
-    let myRef: React.RefObject<HTMLDivElement> = React.createRef();
+interface ICanvasProps {
+    selectedFigure?: SelectedShape
+}
+interface ICanvasState {}
 
+export default class Canvas extends React.Component<ICanvasProps, ICanvasState> {
+// export default function Canvas(props) {
+    // myP5: P5;
+    // myRef: React.RefObject<HTMLDivElement> = React.createRef();
+
+    // static propTypes = {
+    //     selectedFigure: PropTypes.oneOf(Object.keys(SelectedShape)),
+    // };
     /*
      * TODO (delete this and read from props)
      * Mock data 
      */
-    let settings: CanvasSettings = {
-        selectedFigure: props.shapeSelection,
-        selectedAnimation: SelectedAnimation.None,
-        reset: false
-    };
-
-    let Sketch = (p : P5) => {
+    constructor(props) {
+        super(props);
+        // this.myRef = React.createRef();
+    }
+    
+    sketch = (p) => {
         let figs: AnimatedFigure[] = [];
+        let selectedFigure = SelectedShape.None;
 
         p.setup = function () {
-            p.createCanvas(640, 360);
+            p.createCanvas(1000, 500);
             figs = [];
+        }
+        
+        p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
+            if (props.selectedFigure) {
+                selectedFigure = props.selectedFigure;
+            }
         }
         
         p.draw = function () {
             p.background(204);
-
+            p.fill(100);
             figs.forEach(fig => {
                 fig.update(3);
                 fig.display();
             });
-        }
-
-        p.mousePressed = function () {
-            switch(settings.selectedFigure) {
-                case SelectedShape.Circle:
-                    let newCirc = new CircleFigure(p.mouseX, p.mouseY, -0.02, 90, p);
-                    figs.push(newCirc);
-                    break;
-                case SelectedShape.Rectangle:
-                    let newSquare = new SquareFigure(p.mouseX, p.mouseY, -0.02, 90, p);
-                    figs.push(newSquare);
-                    break;
-                case SelectedShape.Triangle: 
-                    let newTriangle = new TriangleFigure(p.mouseX, p.mouseY, -0.02, 90, p);
-                    figs.push(newTriangle);
-                    break;
-            }  
-
-            // prevent default
-            return false;
+            
+            p.mousePressed = function () {
+                switch(selectedFigure) {
+                    case SelectedShape.Circle:
+                        let newCirc = new CircleFigure(p.mouseX, p.mouseY, -0.02, 50, p);
+                        figs.push(newCirc);
+                        break;
+                    case SelectedShape.Rectangle:
+                        let newSquare = new SquareFigure(p.mouseX, p.mouseY, -0.02, 90, p);
+                        figs.push(newSquare);
+                        break;
+                    case SelectedShape.Triangle: 
+                        let newTriangle = new TriangleFigure(p.mouseX, p.mouseY, -0.02, 90, p);
+                        figs.push(newTriangle);
+                        break;
+                }  
+                // prevent default
+                return false;
+            }
         }
     }
+                
+    // componentDidMount() {
+        // this.myP5 = new p5 (this.sketch, this.myRef.current);
+    // }
 
-    myP5 = new p5(Sketch, myRef.current)
-
-    return (
-        <div ref={myRef}>
-
-        </div>
-    );
+    render() {
+        return <P5Wrapper sketch={this.sketch} selectedFigure={this.props.selectedFigure}/>;
+        {/* if (this.myP5){
+            console.log("render myp5");
+            return (<div ref={this.myRef}></div>);
+        } else {
+            return (<div></div>);
+        } */}
+    }
 }
--- src/Views/Draw/Draw.tsx
+++ src/Views/Draw/Draw.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import './Draw.css';
 import Canvas from '../../Components/Canvas/Canvas';
 import ShapesToolbar from '../../Components/ShapesToolbar/ShapesToolbar';
-import { SelectedShape } from "../../Types/Figures";
+import { CanvasSettings, SelectedAnimation, SelectedShape } from "../../Types/Figures";
 
 export default function Draw(){
     const [shapeSelection, setShapeSelection] = useState(SelectedShape.None);
@@ -11,12 +11,19 @@ export default function Draw(){
         if(shapeSelection === selection) setShapeSelection(SelectedShape.None);
         else setShapeSelection(selection);
     }
+
+    // let settings: CanvasSettings = {
+    //   selectedFigure: shapeSelection,
+    //   selectedAnimation: SelectedAnimation.None,
+    //   reset: false
+    // };
+
     return (
       <div>
         <h1> Drawing Container </h1>
         <ShapesToolbar  shapeSelection={shapeSelection}
                         selectionHandler={shapeSelectionHandler} />
-        <Canvas shapeSelection={shapeSelection}/>
+        <Canvas selectedFigure={shapeSelection}/>
      </div>
     );  
     // Render Toolbar and canvas component
