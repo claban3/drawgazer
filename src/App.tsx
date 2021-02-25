8f2b0646434b13648104d8517d7c47ee1b08217b
claban3 <corlaban@umich.edu>
2021-02-24 19:54:26 -0500

    refact header into draw view

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,22 @@ import Draw from './Views/Draw/Draw';
 import Settings from './Views/Settings/Settings';
 import React, { useState } from 'react';
 import { ViewType } from './Types/Views';
-import HeaderToolbar from './Components/HeaderToolbar/Header';
 function App() {
   const [currentView, setCurrentView] = useState(ViewType.Draw);
-  const [clearCanvas, setClearCanvas] = useState(false);
 
   function viewSelectionHandler(event, selection : ViewType) {
     setCurrentView(selection)
-  }
-
-  function setClearCanvasHandler() {
-    setClearCanvas(!clearCanvas);
   }
 
   switch (currentView) {
     case ViewType.Draw: 
       return ( 
         <div> 
-          <HeaderToolbar currentView={currentView} 
-                         resetCanvas={setClearCanvasHandler}
-                         selectionHandler={viewSelectionHandler}/>        
-          <Draw clearCanvas={clearCanvas} resetInParent={setClearCanvasHandler}/>
+          <Draw/>
         </div>);
 
     case ViewType.Settings:
-      return (<div> 
-          <HeaderToolbar currentView={currentView} 
-                         resetCanvas={setClearCanvasHandler}
-                         selectionHandler={viewSelectionHandler}/>        
-          <Settings/> 
-        </div>);
+      return (<div> <Settings/>  </div>);
 
     default: 
         return <div>Error, invalid view</div>
--- src/Components/HeaderToolbar/Header.tsx
+++ src/Components/HeaderToolbar/Header.tsx
@@ -3,29 +3,10 @@ import DeleteIcon from '@material-ui/icons/Delete';
 
 import React from "react";
 import './Header.css';
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import DeleteForever from '@material-ui/core/Icon/';
-
-import { ViewType } from '../../Types/Views';
-import Icon from "@material-ui/core/Icon/";
-import { SvgIcon } from "@material-ui/core";
 
 export default function HeaderToolbar(props) { 
     return (
         <div className="header-container">
-          {/* <Paper className="header-tabs">
-            <Tabs value={props.currentView} 
-                  onChange={props.selectionHandler} 
-                  indicatorColor="primary"
-                  textColor="primary"
-                  // centered
-                  aria-label="simple tabs example">
-              <Tab label="Drawing" value={ViewType.Draw} {...a11yProps(0)} />
-              <Tab label="Settings" value={ViewType.Settings} {...a11yProps(1)} />
-            </Tabs>
-          </Paper> */}
         <h1>drawgazer</h1>
         <IconButton aria-label="delete"
                     className="header-item"
