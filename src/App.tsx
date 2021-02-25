6e0f8fd522552cdaafe283e4cbd1bcd0b649e612
claban3 <corlaban@umich.edu>
2021-02-24 19:55:34 -0500

    remove view changing code for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Draw from './Views/Draw/Draw';
-import Settings from './Views/Settings/Settings';
 import React, { useState } from 'react';
-import { ViewType } from './Types/Views';
+
 function App() {
-  const [currentView, setCurrentView] = useState(ViewType.Draw);
-
-  function viewSelectionHandler(event, selection : ViewType) {
-    setCurrentView(selection)
-  }
-
-  switch (currentView) {
-    case ViewType.Draw: 
-      return ( 
-        <div> 
-          <Draw/>
-        </div>);
-
-    case ViewType.Settings:
-      return (<div> <Settings/>  </div>);
-
-    default: 
-        return <div>Error, invalid view</div>
-  }
+  return <Draw/>;
 }
 
 export default App;
