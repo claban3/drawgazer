03c7531ef5997bc2b204e2089010d3769d674a1c
claban3 <corlaban@umich.edu>
2021-03-09 18:27:57 -0500

    ensure downward gravity doesn't cause objects to go left of page.

--- src/Types/Animations/DownwardGravity.tsx
+++ src/Types/Animations/DownwardGravity.tsx
@@ -3,8 +3,13 @@ import { SketchData } from '../Figures';
 export class DownwardGravity extends Animation {
     static draw(sketchData: SketchData, p) {
         sketchData.figs.forEach(fig => {
+          if(fig.collideCanvasLeft(sketchData.canvasWidth, sketchData.canvasHeight * 2) ||
+             fig.collideCanvasRight(sketchData.canvasWidth, sketchData.canvasHeight * 2)) {
+            fig.velocity.x *= -1;
+          }
+          
           if (!fig.collideCanvasBottom(sketchData.canvasWidth, sketchData.canvasHeight)) {
-            fig.velocity.add(0, 0.10);
+            fig.velocity.add(0, 0.07);
             fig.pos.add(fig.velocity);
             fig.angle += fig.spin;
             // fig.collision.play();
