a9cdbc0718fcbba0df91689bd794ae1a24b15bfd
claban3 <corlaban@umich.edu>
2021-03-21 21:28:05 -0400

    respond to owen's comments

--- src/Types/Animations/WallBounce.tsx
+++ src/Types/Animations/WallBounce.tsx
@@ -3,7 +3,7 @@ import { SketchData } from '../Figures';
 export class WallBounce extends Animation {
     static draw(sketchData: SketchData, p) {
         p.background(255);
-        p.fill(100);
+        
         sketchData.figs.forEach(fig => {
             let width = sketchData.canvasWidth;
             let height = sketchData.canvasHeight;
