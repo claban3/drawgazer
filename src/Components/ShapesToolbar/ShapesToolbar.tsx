ba4a8910d207385b3f628120c51eb30d3662273e
Owen Cannon <owen.cannon1@gmail.com>
2021-03-31 15:22:18 -0400

    more event handlers

--- src/Components/ShapesToolbar/ShapesToolbar.tsx
+++ src/Components/ShapesToolbar/ShapesToolbar.tsx
@@ -27,8 +27,7 @@ export default function ShapesToolbar(props)
     return (
         <div className="shape-toolbar-container">
             <div className={`shape-selection ${triangleHighlight}`}
-                 onClick = { () => props.selectionHandler(SelectedShape.Triangle) }
-                 onTouchEnd = { () => touchEndHandler()}>
+                  onTouchEnd= { () => props.selectionHandler(SelectedShape.Triangle) }>
                 
                 <svg className="shape-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 169.82 147.06">
                     <polygon className="triangle" id="triangle" points="84.91 3 2.6 145.56 167.22 145.56 84.91 3" stroke="#231f20" strokeMiterlimit="10" strokeWidth="3"/>
@@ -37,8 +36,7 @@ export default function ShapesToolbar(props)
             </div>
 
             <div className={`shape-selection ${rectangleHighlight}`}
-                 onClick = { () => props.selectionHandler(SelectedShape.Rectangle) }
-                 onTouchStart = { () => touchStartHandler()}>
+                 onTouchStart = { () => props.selectionHandler(SelectedShape.Rectangle) }>
                 
                 <svg className="shape-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 147 147">
                     <rect className="square "id="square" x="1.5" y="1.5" width="144" height="144" stroke="#231f20" strokeMiterlimit="10" strokeWidth="3"/>
@@ -47,8 +45,7 @@ export default function ShapesToolbar(props)
             </div>
 
             <div className={`shape-selection ${circleHighlight}`}
-                 onClick = { () => props.selectionHandler(SelectedShape.Circle) }
-                 onSelect = { () => onSelectHandler() }>
+                 onFocus = { () => props.selectionHandler(SelectedShape.Circle) }>
 
                 <svg className="shape-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 147 147">
                     <circle className="circle" id="circle" cx="73.5" cy="73.5" r="72" stroke="#231f20" strokeMiterlimit="10" strokeWidth="3"/>
