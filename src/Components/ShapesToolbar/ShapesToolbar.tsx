65856da442a69b1c0c2e6957b41bfb1e3a9b24b2
Owen Cannon <owen.cannon1@gmail.com>
2021-03-31 14:14:13 -0400

    onSelect

--- src/Components/ShapesToolbar/ShapesToolbar.tsx
+++ src/Components/ShapesToolbar/ShapesToolbar.tsx
@@ -44,7 +44,7 @@ export default function ShapesToolbar(props)
 
             <div className={`shape-selection ${circleHighlight}`}
                  onClick = { () => props.selectionHandler(SelectedShape.Circle) }
-                 onTouchEnd = { () => onSelectHandler() }>
+                 onSelect = { () => onSelectHandler() }>
 
                 <svg className="shape-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 147 147">
                     <circle className="circle" id="circle" cx="73.5" cy="73.5" r="72" stroke="#231f20" strokeMiterlimit="10" strokeWidth="3"/>
