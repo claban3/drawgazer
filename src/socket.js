c804481e5250111e3a5cdd9fc7cd68b9a9bcc67f
Owen Cannon <owen.cannon1@gmail.com>
2021-04-20 08:13:41 -0400

    Server URL directed to local device

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,4 @@
 import io from "socket.io-client";
-let socket = io("http://localhost:8000");
+// let socket = io("http://localhost:8000");
+let socket = io("http://192.168.0.17:8000");
 export default socket;
